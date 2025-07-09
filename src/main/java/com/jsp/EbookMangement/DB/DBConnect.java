6bd6aa05683d478a6aee9d941af45f7b9e87c5f1
Mohd Daoud <mohd3daoud@gmail.com>
2025-07-09 23:51:13 +0530

    Initial commit of E-Book Management System project

---
+++ src/main/java/com/jsp/EbookMangement/DB/DBConnect.java
@@ -0,0 +1,31 @@
+package com.jsp.EbookMangement.DB;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class DBConnect {
+
+	private static Connection conn;
+
+	public static Connection getConn() {
+
+		try {
+
+			Class.forName("com.mysql.cj.jdbc.Driver");
+
+			String url = "jdbc:mysql://127.0.0.1:3306/ebook";
+			String user = "root";
+			String pass = "root";
+
+			conn = DriverManager.getConnection(url, user, pass);
+
+		} catch (Exception e) {
+			e.printStackTrace();
+
+		}
+
+		return conn;
+
+	}
+
+}
